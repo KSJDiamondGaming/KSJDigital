decc2926f1d2295f29f1c0343b467a78a14479fc
KSJ DIGITAL <kylemorgan@hotmail.com>
2026-06-09 16:09:53 +0100

    Clean unused KSJ Digital scaffold

--- src/lib/sanity.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const client = createClient({
-  projectId: '4ph8qwbg',
-  dataset: 'production',
-  apiVersion: '2026-04-30',
-  useCdn: true,
-});
